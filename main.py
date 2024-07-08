1a527e2288180b969bd167ed665038b39abed644
chrisda28 <chrisda28@gmail.com>
2024-07-08 13:45:02 -0400

    create dataframe from pulled stock data and clean the data frame

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ import os
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), 'key.env')   # specifies the path to my environment var
-
-
 load_dotenv(dotenv_path)
 
-
-
 api_key = os.getenv('apikey')
-print(api_key)
-
 INTERESTED_STOCKS = ["JPM",]  # add more here
+endpoint = "https://www.alphavantage.co/query"
 
 parameters = {
         "function": "TIME_SERIES_DAILY",
@@ -22,3 +17,14 @@ parameters = {
         "outputsize": "full",
         "apikey": api_key
     }
+
+response = requests.get(endpoint, params=parameters)
+response.raise_for_status()    # used for https errors
+json_data = response.json()
+
+df = pd.DataFrame.from_dict(json_data)
+# df.rename(columns={})
+trimmed_top_rows = df.iloc[5:] # deletes the first 5 rows
+deleted_column_one = trimmed_top_rows.iloc[:, 1:]  # deletes meta data column
+final_df = deleted_column_one.rename(columns={"Time Series (Daily)": "JPM Daily Time Series"})  # rename column
+print(final_df)
